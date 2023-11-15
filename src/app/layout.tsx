3fc63acfa6e067646a45d5cb3611f3d5ea740612
Glaucielle Sá <glauciellesa@gmail.com>
2023-11-15 22:02:10 +0100

    add header and footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Navabar from '@/components/Navabar'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +20,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.variable} font-sans`} suppressHydrationWarning={true}>
+        <header><Navabar /></header>
+        <main>
+          {children}
+        </main>
+        <footer className='text-center p-5'>By Glaucielle</footer>
+      </body>
     </html>
   )
 }
---
+++ src/components/Navabar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navabar = () => {
+  return (
+    <ul className='flex gap-3 justify-end p-5'>
+      <li>Home</li>
+      <li>About us</li>
+      <li>More</li>
+      <li>Login</li>
+    </ul>
+  );
+};
+
+export default Navabar;
